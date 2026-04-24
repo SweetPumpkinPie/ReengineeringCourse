df4524dd682ae9a962da6b8f856416b0afb3013a
SweetPumpkinPie <oleksandrgolub@knu.ua>
2026-04-24 16:56:38 +0300

    test: add an architectural rule that checks for the absence of modules that should not be used together

--- NetSdrClientAppTests/ArchitectureTests.cs
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -1,6 +1,25 @@
-﻿namespace NetSdrClientAppTests;
+﻿using NetArchTest.Rules;
+using NetSdrClientApp;
+
+namespace NetSdrClientAppTests;
 
 public class ArchitectureTests
 {
-    
+    [Test]
+    public void MessagesNamespace_ShouldNot_DependOnNetworking()
+    {
+        // Arrange
+        var assembly = typeof(NetSdrClient).Assembly;
+
+        // Act
+        var result = Types.InAssembly(assembly)
+            .That()
+            .ResideInNamespace("NetSdrClientApp.Messages")
+            .ShouldNot()
+            .HaveDependencyOn("NetSdrClientApp.Networking")
+            .GetResult();
+
+        // Assert
+        Assert.That(result.IsSuccessful, Is.True, "An architectural rule has been violated! The Messages module must not depend on Networking.");
+    }
 }
