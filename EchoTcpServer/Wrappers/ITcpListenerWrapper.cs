08c8c3a15b676e003f0540c9f592f8c99a6ed676
SweetPumpkinPie <oleksandrgolub@knu.ua>
2026-05-12 23:58:15 +0300

    refactor: add `ITcpListenerWrapper.cs`

---
+++ EchoTcpServer/Wrappers/ITcpListenerWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+public interface ITcpListenerWrapper
+{
+    void Start();
+    void Stop();
+    Task<ITcpClientWrapper> AcceptTcpClientAsync();
+}
