a52a94efe86a70c73516fe16a386eabd1d7351a5
SweetPumpkinPie <oleksandrgolub@knu.ua>
2026-05-13 00:02:29 +0300

    refactor: add `RealTcpListenerWrapper.cs`

---
+++ EchoTcpServer/Wrappers/RealTcpListenerWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+public class RealTcpListenerWrapper : ITcpListenerWrapper
+{
+    private readonly TcpListener _listener;
+    
+    public RealTcpListenerWrapper(int port)
+    {
+        _listener = new TcpListener(IPAddress.Any, port);
+    }
+
+    public void Start() => _listener.Start();
+    public void Stop() => _listener.Stop();
+    
+    public async Task<ITcpClientWrapper> AcceptTcpClientAsync()
+    {
+        var client = await _listener.AcceptTcpClientAsync();
+        return new RealTcpClientWrapper(client);
+    }
+}
