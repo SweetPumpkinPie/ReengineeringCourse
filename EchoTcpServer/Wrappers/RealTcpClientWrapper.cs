8584a1e32a024f98ad5699117a4f6530c71a810c
SweetPumpkinPie <oleksandrgolub@knu.ua>
2026-05-13 00:01:22 +0300

    refactor: add `RealTcpClientWrapper.cs`

---
+++ EchoTcpServer/Wrappers/RealTcpClientWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+public class RealTcpClientWrapper : ITcpClientWrapper
+{
+    private readonly TcpClient _client;
+    public RealTcpClientWrapper(TcpClient client) => _client = client;
+    public NetworkStream GetStream() => _client.GetStream();
+    public void Close() => _client.Close();
+}
