2c83bcaf7f02b52c5346ad9912dfb11743b664ef
SweetPumpkinPie <oleksandrgolub@knu.ua>
2026-05-18 15:56:15 +0300

    refactor: validation before iteration

--- NetSdrClientApp/Messages/NetSdrMessageHelper.cs
+++ NetSdrClientApp/Messages/NetSdrMessageHelper.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace NetSdrClientApp.Messages
 {
-    //TODO: analyze possible use of [StructLayout] for better performance and readability 
     public static class NetSdrMessageHelper
     {
         private const short _maxMessageLength = 8191;
@@ -108,23 +107,28 @@ namespace NetSdrClientApp.Messages
 
         public static IEnumerable<int> GetSamples(ushort sampleSize, byte[] body)
         {
-            sampleSize /= 8; //to bytes
-            if (sampleSize  > 4)
+            int sampleSizeBytes = sampleSize / 8; //to bytes
+            if (sampleSize > 4)
             {
-                throw new ArgumentOutOfRangeException();
+                throw new ArgumentOutOfRangeException(nameof(sampleSize),
+                    "Sample size must be 32 bits (4 bytes) or less.");
             }
 
-            var bodyEnumerable = body as IEnumerable<byte>;
-            var prefixBytes = Enumerable.Range(0, 4 - sampleSize)
-                                      .Select(b => (byte)0);
+            return GetSamplesInternal(sampleSizeBytes, body);
+        }
 
-            while (bodyEnumerable.Count() >= sampleSize)
+        private static IEnumerable<int> GetSamplesInternal(int sampleSizeBytes, byte[] body)
+        {
+            var bodyEnumerable = body as IEnumerable<byte>;
+            var prefixBytes = Enumerable.Range(0, 4 - sampleSizeBytes).Select(b => (byte)0);
+
+            while (bodyEnumerable.Count() >= sampleSizeBytes)
             {
                 yield return BitConverter.ToInt32(bodyEnumerable
-                    .Take(sampleSize)
+                    .Take(sampleSizeBytes)
                     .Concat(prefixBytes)
                     .ToArray());
-                bodyEnumerable = bodyEnumerable.Skip(sampleSize);
+                bodyEnumerable = bodyEnumerable.Skip(sampleSizeBytes);
             }
         }
 
